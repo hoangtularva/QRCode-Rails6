37f4949cb385fe18eaace0d2462c84bf9187768f
hoangtularva <hoangtukg295@gmail.com>
2020-08-31 13:48:46 +0700

    rails g controller qr_codes new create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'qr_codes/new'
+  get 'qr_codes/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
