3e4b68de8c10959b6c30d5c7d64a0ad81ea17189
hoangtularva <hoangtukg295@gmail.com>
2020-08-31 14:00:41 +0700

    rails webpacker:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :qr_codes, only: [:new, :create]
-  root to: "qr_codes#new"
+   resources :qr_codes, only: [:new, :create]
+   root to: "qr_codes#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
